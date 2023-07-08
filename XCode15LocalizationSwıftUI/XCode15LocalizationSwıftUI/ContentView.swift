dfef7548b937742c42bccec70bbd48493750e44a
İbrahim Demirci <75900229+ibrahim-demirci@users.noreply.github.com>
2023-07-08 23:59:48 +0300

    Upload files.

---
+++ XCode15LocalizationSwıftUI/XCode15LocalizationSwıftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCode15LocalizationSwıftUI
+//
+//  Created by İbrahim Demirci on 8.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCode15LocalizationSwıftUI/XCode15LocalizationSwıftUI/XCode15LocalizationSw_ftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCode15LocalizationSw_ftUIApp.swift
+//  XCode15LocalizationSwıftUI
+//
+//  Created by İbrahim Demirci on 8.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct XCode15LocalizationSw_ftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
